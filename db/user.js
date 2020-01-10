9946ccfb94e7f48d34bcdb93bb97c8ed119b720b
jwayn <prickettjw@gmail.com>
2020-01-10 00:56:43 -0800

    Login system working with features

--- db/user.js
+++ db/user.js
@@ -1,8 +1,9 @@
 const knex = require('./connection');
+const moment = require('moment');
 
 module.exports = {
     getOneById: function(id) {
-        return knex('users').where('id', id).first();
+        return knex('users').select('id', 'username', 'email', 'created', 'last_login').where({id}).first();
     },
     getOneByEmail: function(email) {
         return knex('users').where('email', email).first();
@@ -20,6 +21,37 @@ module.exports = {
         return knex('users').where({id}).update(user);
     },
     updateLastLogin: function(id) {
-        return knex('users').where({id}).update({last_login: Date.now()})
-    }
+        console.log('Updating last login');
+        console.log(moment.utc(Date.now()).format());
+        return knex('users').where({id}).update({last_login: moment.utc(Date.now()).format()})
+    },
+    createVerification: function(user_id, token) {
+        console.log('Creating verification');
+        return knex('users_verification').insert({user_id, token}).returning('token');
+    },
+    getVerificationByToken(token) {
+        console.log('Grabbing verification record for "' + token + '" .');
+        return knex('users_verification').where({token}).first();
+    },
+    deleteVerificationRecord(user_id) {
+        console.log('Deleting verification record for user_id "' + user_id + '".');
+        return knex('users_verification').where({user_id}).delete();
+    },
+    createResetPassToken: function(user_id, token) {
+        console.log('Creating reset pass token');
+        return knex('users_reset_pass').insert({user_id, token});
+    },
+    getResetPassToken: function(token) {
+        return knex('users_reset_pass').where({token}).first();
+    },
+    getResetPassTokenByUser: function(user_id) {
+        return knex('users_reset_pass').where({user_id}).first();
+    },
+    deleteResetPassToken: function(token) {
+        console.log('Deleting reset pass record with token ' + token);
+        return knex('users_reset_pass').where({token}).delete();
+    },
+    delete: function(id) {
+        return knex('users').where({id}).delete();
+    },
 };
--- routes/users.js
+++ routes/users.js
@@ -3,17 +3,57 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const createError = require('http-errors');
 const uuidv4 = require('uuid/v4');
+const verifyToken = require('../middleware/verify-token');
+const nodemailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport');
+
 require('dotenv').config();
 
 const User = require('../db/user')
 
 const router = express.Router();
 
+const auth = {
+    auth: {
+      api_key: process.env.MG_API_KEY,
+      domain: process.env.MG_DOMAIN,
+    },
+  }
+
+const nodemailerMailgun = nodemailer.createTransport(mg(auth));
+ 
+function sendEmail(recipient, subject, html, text) {
+    nodemailerMailgun.sendMail({
+        from: 'Roll Initiative <info@rollinitiative.app>',
+        to: recipient,
+        subject,
+        html: html ? html : '',
+        text,
+    }, (err, info) => {
+        if (err) {
+            console.log(`Error: ${err}`);
+        }
+        else {
+            console.log(`Response: ${info}`);
+        }
+    });
+}
+
 
 // Function for testing slow api response
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+// Generate pseudo-random strings
+function generateToken(length) {
+    let token = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for(let i = 0; i < length; i++) {
+        token += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return token;
+};
 
 
 //Ensure that email/password is valid
@@ -32,31 +72,142 @@ router.post('/signin', async (req, res, next) => {
         //await sleep(3000);
         if(validUser(req.body)) {
             const user = await User.getOneByEmail(req.body.email);
+            console.log(user);
             if(user) {
-                const result = await bcrypt.compare(req.body.password, user.password);
-                // If the password matches
-                if(result){
-                    User.updateLastLogin(user.id);
-                    console.log(process.env.JWT_SECRET)
-                    if(process.env.JWT_SECRET) {
-                        jwt.sign({user_id: user.id, username: user.username, email: user.email}, process.env.JWT_SECRET, {expiresIn: '1d'}, (err, token) => {
-                            res.json({
-                                token
+                //If the user is active
+                if(user.active === true) {
+                    const result = await bcrypt.compare(req.body.password, user.password);
+                    // If the password matches
+                    if(result){
+                        await User.updateLastLogin(user.id);
+                        console.log(process.env.JWT_SECRET)
+                        if(process.env.JWT_SECRET) {
+                            jwt.sign({user_id: user.id, username: user.username, email: user.email}, process.env.JWT_SECRET, {expiresIn: '1d'}, (err, token) => {
+                                res.json({
+                                    token
+                                });
                             });
-                        });
+                        } else {
+                            next(createError(500))
+                        }
                     } else {
-                        next(createError(500))
+                        next(createError(401, 'Incorrect email or password'));
                     }
                 } else {
-                    next(createError(401, 'Incorrect email or password'));
-                }
+                    next(createError(401, 'Account is not verified.'));
+                };
             } else {
                 next(createError(401, 'A user with that email does not exist.'));
-            };
+            }
+        } else {
+            next(createError(401, 'Incorrect email or password.'))
         };
     } catch (err) {
         next(err)
     }
+});
+
+// Get user
+router.get('/', verifyToken, async (req, res, next) => {
+    if(req.tokenData) {
+        const user = await User.getOneById(req.tokenData.user_id);
+        console.log(user);
+        res.json({user});
+    } else {
+        next(createError(401));
+    }
+});
+
+router.post('/requestpassreset', async (req, res, next) => {
+    console.log(req.body);
+    try {
+        if(req.body.email) {
+            console.log('Checking if user exists.')
+            // Check if user exists in users by email
+            const user = await User.getOneByEmail(req.body.email).catch(err => {next(err)});
+            if(user) {
+                console.log(user);
+                // Create a record in the reset pass table with an auth key.
+                const token = generateToken(40);
+                //Check to see if user have a token already
+                const existingToken = await User.getResetPassTokenByUser(user.id);
+                //If they do, delete it.
+                if(existingToken) {
+                    console.log(existingToken);
+                    await User.deleteResetPassToken(existingToken.token);
+                };
+                //Insert a pass reset record in DB
+                await User.createResetPassToken(user.id, token).catch(err => {next(err)});
+                // Email the user a link to the frontend 
+                const link = `${req.protocol}://${req.headers.host}/resetpassword?token=${token}`;
+                sendEmail(
+                    `${user.username} <${user.email}>`,
+                    'Reset your RollInitiative password.',
+                    `Please click <a href="${link}">here</a> to reset your password.`,
+                    `Please click ${link} to reset your password.`
+                );
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(204);
+            }
+    
+        }
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.post('/changepass', async (req, res, next) => {
+    if(req.query.token) {
+        //Check the database for that token
+        const resetRecord = await User.getResetPassToken(req.query.token);
+        if(resetRecord) {
+            if(req.body.password) {
+                const hash = await bcrypt.hash(req.body.password, 10).catch(err => {next(err)});
+                await User.update(resetRecord.user_id, {password: hash}).catch(err => {next(err)});
+                await User.deleteResetPassToken(resetRecord.token);
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(402);
+            }
+        } else {
+            res.sendStatus(401);
+        }
+    }
+});
+
+router.get('/verify', async (req, res, next) => {
+    if(req.query.token) {
+        //check for token in database
+        const verificationRecord = await User.getVerificationByToken(req.query.token).catch(err => {next(err)});
+
+        if(verificationRecord) {
+            await User.update(verificationRecord.user_id, {active: true}).catch(err => {next(err)});
+            await User.deleteVerificationRecord(verificationRecord.user_id);
+            // Log user in
+            const user = await User.getOneById(verificationRecord.user_id).catch(err => {next(err)});
+            if(user) {
+                await User.updateLastLogin(user.id);
+                console.log(user);
+                jwt.sign({user_id: user.id, username: user.username, email: user.email}, process.env.JWT_SECRET, {expiresIn: '1d'}, (err, token) => {
+                    console.log('Logging in user with email "' + user.email + '".')
+                    // If we were successful we log return the JWT token
+                    res.json({
+                        token
+                    });
+                });
+            } else {
+                res.send(402)
+            }
+        } else {
+            res.send(401);
+        }
+    }
+});
+
+router.delete('/', verifyToken, async (req, res, next) => {
+    await User.delete(req.tokenData.user_id).catch(err => {next(err)});
+    res.send(200);
 });
 
 router.post('/signup', async (req, res, next) => {
@@ -74,11 +225,21 @@ router.post('/signup', async (req, res, next) => {
 
             const newUser = await User.create(user);
 
-            await jwt.sign({user_id: newUser.id, email: newUser.email, username: newUser.username}, process.env.JWT_SECRET, {expiresIn: '1d'}, (err, token) => {
-                res.json({
-                    token
-                });
-            });
+            const verificationToken = await User.createVerification(newUser.id, generateToken(40));
+            console.log(verificationToken);
+            await sendEmail(
+                `${newUser.username} <${newUser.email}>`,
+                'Please verify your RollInitiative account.',
+                `Please <a href="${req.protocol}://${req.headers.host}/verify?token=${verificationToken}">verify your RollInitiative account</a>.`,
+                `Please verify your RollInitiative account at ${req.protocol}://${req.headers.host}/verify?token=${verificationToken}`
+            );
+
+            res.sendStatus(200);
+            // await jwt.sign({user_id: newUser.id, email: newUser.email, username: newUser.username}, process.env.JWT_SECRET, {expiresIn: '1d'}, (err, token) => {
+            //     res.json({
+            //         token
+            //     });
+            // });
 
         } else {
             next(createError(401, 'A user with that email address already exists.'))
